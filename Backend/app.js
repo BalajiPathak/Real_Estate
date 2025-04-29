72abfa4223a5bd3175c442c70376e47fb4c6ebd2
Manisha Soni <manishasoni@startbitsolutions.com>
2025-04-29 17:41:27 +0530

    addign  blog

--- Backend/app.js
+++ Backend/app.js
@@ -33,6 +33,7 @@ const storage = multer.diskStorage({
 });
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // Create uploads directory if it doesn't exist
 const fs = require('fs');
 if (!fs.existsSync('uploads')) {
@@ -63,6 +64,9 @@ app.use(session({
 }));
 
 // Add this middleware to make user data available globally
+=======
+
+>>>>>>> Stashed changes
 =======
 
 >>>>>>> Stashed changes
@@ -126,6 +130,9 @@ const storage = multer.diskStorage({
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
@@ -288,6 +295,7 @@ passport.use(new FacebookStrategy({
 }));
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 app.use(errorHandler.handle404);
 
 app.use(errorHandler.handle500);
@@ -302,6 +310,9 @@ app.use(authenticateToken);
 // require('./models/propertyData');
 
 const PORT =3006;
+=======
+const PORT = process.env.PORT || 3006;
+>>>>>>> Stashed changes
 =======
 const PORT = process.env.PORT || 3006;
 >>>>>>> Stashed changes
