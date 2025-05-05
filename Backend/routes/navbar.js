751575394c66c646479a98fe509ceeb9f0aa7dc8
BalajiPathak <balajipathak@startbitsolutions.com>
2025-05-05 09:35:42 +0530

    cpatcha added

--- Backend/routes/navbar.js
+++ Backend/routes/navbar.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const navController = require('../controllers/navbar');
 const { check } = require('express-validator');
+const axios = require('axios');
 
 router.post('/navbar', navController.createNavbar);
 
@@ -10,27 +11,24 @@ router.get('/contact', navController.getContact);
 router.post(
     '/contact',
     [
-        check('firstname')
-            .trim()
-            .notEmpty()
-            .withMessage('First name is required'),
-        check('lastname')
-            .trim()
-            .notEmpty()
-            .withMessage('Last name is required'),
-        check('email')
-            .isEmail()
-            .withMessage('Please enter a valid email'),
-        check('subject')
-            .trim()
-            .notEmpty()
-            .withMessage('Subject is required'),
+        check('firstname').trim().notEmpty().withMessage('First name is required'),
+        check('lastname').trim().notEmpty().withMessage('Last name is required'),
+        check('email').isEmail().withMessage('Please enter a valid email'),
+        check('subject').trim().notEmpty().withMessage('Subject is required'),
         check('message')
             .trim()
             .notEmpty()
             .withMessage('Message is required')
             .isLength({ min: 10 })
-            .withMessage('Message must be at least 10 characters long')
+            .withMessage('Message must be at least 10 characters long'),
+        check('captchaInput')
+            .trim()
+            .custom((value, { req }) => {
+                if (value !== req.body.captchaText) {
+                    throw new Error('CAPTCHA invalid ');
+                }
+                return true;
+            })
     ],
     navController.postContact
 );
