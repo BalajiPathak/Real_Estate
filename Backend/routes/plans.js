44c50a87cd6cee58f145ab89281701c136149cf4
Manisha Soni <manishasoni@startbitsolutions.com>
2025-05-27 17:14:10 +0530

    commit

--- Backend/routes/plans.js
+++ Backend/routes/plans.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const plansController = require('../controllers/plans');
 const isAuth = require('../middleware/is-auth');
 
-// Remove the '/plans' prefix from routes since it's already handled by the app.use(planRoutes)
 router.get('/plans', isAuth, plansController.getPlans);
 router.get('/plans/purchase/:id', isAuth, plansController.getPurchaseForm);
 router.post('/plans/purchase/:id', isAuth, plansController.purchasePlan);
