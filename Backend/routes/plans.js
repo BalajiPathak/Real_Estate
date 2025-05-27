f98fd5f477b56bab97ca0bc364e5672fb4630d44
Manisha Soni <manishasoni@startbitsolutions.com>
2025-05-27 16:43:19 +0530

    commit

--- Backend/routes/plans.js
+++ Backend/routes/plans.js
@@ -7,5 +7,6 @@ const isAuth = require('../middleware/is-auth');
 router.get('/plans', isAuth, plansController.getPlans);
 router.get('/plans/purchase/:id', isAuth, plansController.getPurchaseForm);
 router.post('/plans/purchase/:id', isAuth, plansController.purchasePlan);
+router.get('/plans/purchase/success/:id', isAuth, plansController.purchaseSuccess);
 router.post('/plans/purchase/success/:id', isAuth, plansController.activateFreePlan);
 module.exports = router;
